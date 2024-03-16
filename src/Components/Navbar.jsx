00866375e2a51c40f2865d9afd578e233c146d42
M.Lalikth kumar <121875934+Lalithkumar-19@users.noreply.github.com>
2024-03-16 21:16:07 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,9 +22,13 @@ function Navbar() {
                 {nav ? <AiOutlineClose size={20} /> :
                     <AiOutlineMenu size={20} />}
             </div>
-            <div className={nav ? `fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000300] ease-out duration-500` : `fixed left-[-100%]`} >
+            <div className={nav ? `fixed left-0 top-0 w-[50%]  h-full border-r-gray-900 bg-[#000300] ease-out duration-500` : `fixed left-[-100%]`} >
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>LALITH DEV.</h1>
                 <ul className='p-4 uppercase'>
+                     <div onClick={handleNav} className='block md:hidden sm:block'>
+                {nav ? <AiOutlineClose size={20} /> :
+                    <AiOutlineMenu size={20} />}
+                    
                     <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#education'>Education</AnchorLink></li>
                     <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#skills'>Tech_stack</AnchorLink></li>
                     <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#services' >Services</AnchorLink></li>
