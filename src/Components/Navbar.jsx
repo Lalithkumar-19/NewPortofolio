472c2b3012f64322e59deef6a1a16f4a6093ad40
Lalith Kumar <lalithkumar19180@gmail.com>
2024-04-13 21:47:45 +0530

    new projects update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,11 +18,13 @@ function Navbar() {
                 <li className='p-4  hover:underline underline-offset-4 '><AnchorLink href='#msg_me'>msg_me</AnchorLink></li>
                 <li className='p-4 hover:underline underline-offset-4 '><AnchorLink href='#portofolio'>portofolio</AnchorLink></li>
             </ul>
+
             <div onClick={handleNav} className='block md:hidden '>
                 {nav ? <AiOutlineClose size={20} /> :
                     <AiOutlineMenu size={20} />}
             </div>
-            <div className={nav ? `fixed left-0 top-0 w-[50%]  h-full border-r-gray-900 bg-[#000300] ease-out duration-500` : `fixed left-[-100%]`} >
+
+            <div className={nav ? `fixed left-0 top-0 w-[40%]  h-full border-r-gray-900 bg-[#000300] ease-out duration-500` : `fixed left-[-100%]`} >
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>LALITH DEV.</h1>
                 <ul className='p-4 uppercase'>
                     <div onClick={handleNav} className='block md:hidden sm:block'>
@@ -34,7 +36,7 @@ function Navbar() {
                         <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#services' >Services</AnchorLink></li>
                         <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#social'>Social_media</AnchorLink></li>
                         <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#msg_me'>msg_me</AnchorLink></li>
-                        <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#portofolio'>portofolio</AnchorLink></li>\
+                        <li className='p-4 border-b border-gray-600' onClick={() => { setNav(false) }}><AnchorLink href='#portofolio'>portofolio</AnchorLink></li>
                     </div>
 
                 </ul>
