c5a45f24f21ca6506b082c9801aeb7763af040f4
Lalith Kumar <lalithkumar19180@gmail.com>
2024-04-13 21:54:25 +0530

    new projects update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
                     <AiOutlineMenu size={20} />}
             </div>
 
-            <div className={nav ? `fixed left-0 top-0 w-[40%]  h-full border-r-gray-900 bg-[#000300] ease-out duration-500` : `fixed left-[-100%]`} >
+            <div className={nav ? `fixed left-0 top-0 w-[60%]  h-full border-r-gray-900 bg-[#000300] ease-out duration-500` : `fixed left-[-100%]`} >
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>LALITH DEV.</h1>
                 <ul className='p-4 uppercase'>
                     <div onClick={handleNav} className='block md:hidden sm:block'>
